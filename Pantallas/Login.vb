d5ea6daa1cc9dcfdadb1ceffb97f38992b3c03e1
Ermis <ermis993@gmail.com>
2020-01-19 19:27:37 -0600

    Cambios en el diseño de la pantalla

--- Pantallas/Login.vb
+++ Pantallas/Login.vb
@@ -62,7 +62,12 @@ Public Class Login
     End Function
 
     Private Sub LBL_REGISTRARSE_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LBL_REGISTRARSE.LinkClicked
-
+        Try
+            RegistreseAqui.Show()
+            Me.Visible = False
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 
     Private Sub TXT_CONTRASENA_TextChanged(sender As Object, e As EventArgs) Handles TXT_CONTRASENA.TextChanged
--- Pantallas/RegistreseAqui.Designer.vb
+++ Pantallas/RegistreseAqui.Designer.vb
@@ -39,93 +39,108 @@ Partial Class RegistreseAqui
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(81, 42)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.Label1.ForeColor = System.Drawing.Color.Silver
+        Me.Label1.Location = New System.Drawing.Point(36, 42)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(62, 17)
+        Me.Label1.Size = New System.Drawing.Size(91, 24)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Nombre:"
         '
         'TXT_NOMUSUARIO
         '
-        Me.TXT_NOMUSUARIO.Location = New System.Drawing.Point(201, 42)
+        Me.TXT_NOMUSUARIO.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.TXT_NOMUSUARIO.Location = New System.Drawing.Point(151, 42)
         Me.TXT_NOMUSUARIO.Name = "TXT_NOMUSUARIO"
-        Me.TXT_NOMUSUARIO.Size = New System.Drawing.Size(230, 22)
+        Me.TXT_NOMUSUARIO.Size = New System.Drawing.Size(260, 29)
         Me.TXT_NOMUSUARIO.TabIndex = 1
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(75, 140)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.Label2.ForeColor = System.Drawing.Color.Silver
+        Me.Label2.Location = New System.Drawing.Point(28, 140)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(68, 17)
-        Me.Label2.TabIndex = 2
+        Me.Label2.Size = New System.Drawing.Size(99, 24)
+        Me.Label2.TabIndex = 4
         Me.Label2.Text = "Teléfono:"
         '
         'TXT_TELEFONO
         '
-        Me.TXT_TELEFONO.Location = New System.Drawing.Point(201, 140)
+        Me.TXT_TELEFONO.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.TXT_TELEFONO.Location = New System.Drawing.Point(151, 140)
         Me.TXT_TELEFONO.Name = "TXT_TELEFONO"
-        Me.TXT_TELEFONO.Size = New System.Drawing.Size(100, 22)
-        Me.TXT_TELEFONO.TabIndex = 3
+        Me.TXT_TELEFONO.Size = New System.Drawing.Size(169, 29)
+        Me.TXT_TELEFONO.TabIndex = 5
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(72, 182)
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.Label3.ForeColor = System.Drawing.Color.Silver
+        Me.Label3.Location = New System.Drawing.Point(22, 182)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(71, 17)
-        Me.Label3.TabIndex = 4
+        Me.Label3.Size = New System.Drawing.Size(105, 24)
+        Me.Label3.TabIndex = 6
         Me.Label3.Text = "Direccion:"
         '
         'TXT_DIRECCION
         '
-        Me.TXT_DIRECCION.Location = New System.Drawing.Point(201, 182)
+        Me.TXT_DIRECCION.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.TXT_DIRECCION.Location = New System.Drawing.Point(151, 182)
         Me.TXT_DIRECCION.Multiline = True
         Me.TXT_DIRECCION.Name = "TXT_DIRECCION"
-        Me.TXT_DIRECCION.Size = New System.Drawing.Size(230, 64)
-        Me.TXT_DIRECCION.TabIndex = 5
+        Me.TXT_DIRECCION.Size = New System.Drawing.Size(260, 113)
+        Me.TXT_DIRECCION.TabIndex = 7
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(88, 270)
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.Label4.ForeColor = System.Drawing.Color.Silver
+        Me.Label4.Location = New System.Drawing.Point(47, 317)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(55, 17)
-        Me.Label4.TabIndex = 6
+        Me.Label4.Size = New System.Drawing.Size(80, 24)
+        Me.Label4.TabIndex = 8
         Me.Label4.Text = "Correo:"
         '
         'TXT_CORREO
         '
-        Me.TXT_CORREO.Location = New System.Drawing.Point(201, 270)
+        Me.TXT_CORREO.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.TXT_CORREO.Location = New System.Drawing.Point(151, 317)
         Me.TXT_CORREO.Name = "TXT_CORREO"
-        Me.TXT_CORREO.Size = New System.Drawing.Size(230, 22)
-        Me.TXT_CORREO.TabIndex = 7
+        Me.TXT_CORREO.Size = New System.Drawing.Size(260, 29)
+        Me.TXT_CORREO.TabIndex = 9
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(58, 93)
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.Label5.ForeColor = System.Drawing.Color.Silver
+        Me.Label5.Location = New System.Drawing.Point(5, 93)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(85, 17)
-        Me.Label5.TabIndex = 8
+        Me.Label5.Size = New System.Drawing.Size(122, 24)
+        Me.Label5.TabIndex = 2
         Me.Label5.Text = "Contraseña:"
         '
         'TXT_CONTRASENA
         '
-        Me.TXT_CONTRASENA.Location = New System.Drawing.Point(201, 93)
+        Me.TXT_CONTRASENA.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold)
+        Me.TXT_CONTRASENA.Location = New System.Drawing.Point(151, 93)
         Me.TXT_CONTRASENA.Name = "TXT_CONTRASENA"
         Me.TXT_CONTRASENA.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TXT_CONTRASENA.Size = New System.Drawing.Size(230, 22)
-        Me.TXT_CONTRASENA.TabIndex = 9
+        Me.TXT_CONTRASENA.Size = New System.Drawing.Size(260, 29)
+        Me.TXT_CONTRASENA.TabIndex = 3
         Me.TXT_CONTRASENA.UseSystemPasswordChar = True
         '
         'BTN_GUARDAR
         '
         Me.BTN_GUARDAR.BackgroundImage = Global.VentaRepuestos.My.Resources.Resources.guardar
         Me.BTN_GUARDAR.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.BTN_GUARDAR.Location = New System.Drawing.Point(163, 339)
+        Me.BTN_GUARDAR.Location = New System.Drawing.Point(245, 372)
         Me.BTN_GUARDAR.Name = "BTN_GUARDAR"
-        Me.BTN_GUARDAR.Size = New System.Drawing.Size(75, 70)
+        Me.BTN_GUARDAR.Size = New System.Drawing.Size(75, 66)
         Me.BTN_GUARDAR.TabIndex = 10
         Me.BTN_GUARDAR.UseVisualStyleBackColor = True
         '
@@ -133,9 +148,9 @@ Partial Class RegistreseAqui
         '
         Me.Button2.BackgroundImage = Global.VentaRepuestos.My.Resources.Resources.salir
         Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.Button2.Location = New System.Drawing.Point(287, 339)
+        Me.Button2.Location = New System.Drawing.Point(336, 372)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 70)
+        Me.Button2.Size = New System.Drawing.Size(75, 66)
         Me.Button2.TabIndex = 11
         Me.Button2.UseVisualStyleBackColor = True
         '
@@ -143,7 +158,9 @@ Partial Class RegistreseAqui
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(473, 450)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(104, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(425, 450)
+        Me.ControlBox = False
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.BTN_GUARDAR)
         Me.Controls.Add(Me.TXT_CONTRASENA)
@@ -156,10 +173,11 @@ Partial Class RegistreseAqui
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TXT_NOMUSUARIO)
         Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "RegistreseAqui"
-        Me.Text = "Registrese Aquí"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.ResumeLayout(False)
         Me.PerformLayout()
 

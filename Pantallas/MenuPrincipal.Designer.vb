55291d19ce5de066a778521d5dc3bb6b14088b56
Ermis <ermis993@gmail.com>
2020-01-26 20:04:29 -0600

    C

--- Pantallas/MenuPrincipal.Designer.vb
+++ Pantallas/MenuPrincipal.Designer.vb
@@ -28,7 +28,13 @@ Partial Class MenuPrincipal
         Me.BTN_SALIR = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.CMB_SUCURSAL = New System.Windows.Forms.ComboBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TXT_COMPRA = New System.Windows.Forms.TextBox()
+        Me.TXT_VENTA = New System.Windows.Forms.TextBox()
         Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'BTN_USUARIO
@@ -86,7 +92,7 @@ Partial Class MenuPrincipal
         Me.GroupBox1.Controls.Add(Me.CMB_SUCURSAL)
         Me.GroupBox1.Location = New System.Drawing.Point(1, 516)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(267, 76)
+        Me.GroupBox1.Size = New System.Drawing.Size(267, 84)
         Me.GroupBox1.TabIndex = 4
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "[ Sucursal ]"
@@ -100,12 +106,60 @@ Partial Class MenuPrincipal
         Me.CMB_SUCURSAL.Size = New System.Drawing.Size(255, 24)
         Me.CMB_SUCURSAL.TabIndex = 0
         '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TXT_VENTA)
+        Me.GroupBox2.Controls.Add(Me.TXT_COMPRA)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(293, 516)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(331, 84)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "[ Tipo cambio ]"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Compra :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Venta :"
+        '
+        'TXT_COMPRA
+        '
+        Me.TXT_COMPRA.Enabled = False
+        Me.TXT_COMPRA.Location = New System.Drawing.Point(92, 22)
+        Me.TXT_COMPRA.Name = "TXT_COMPRA"
+        Me.TXT_COMPRA.Size = New System.Drawing.Size(100, 22)
+        Me.TXT_COMPRA.TabIndex = 2
+        '
+        'TXT_VENTA
+        '
+        Me.TXT_VENTA.Enabled = False
+        Me.TXT_VENTA.Location = New System.Drawing.Point(92, 51)
+        Me.TXT_VENTA.Name = "TXT_VENTA"
+        Me.TXT_VENTA.Size = New System.Drawing.Size(100, 22)
+        Me.TXT_VENTA.TabIndex = 3
+        '
         'MenuPrincipal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1188, 600)
+        Me.ClientSize = New System.Drawing.Size(1204, 645)
         Me.ControlBox = False
+        Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.BTN_SALIR)
         Me.Controls.Add(Me.BTN_USUARIO)
@@ -117,6 +171,8 @@ Partial Class MenuPrincipal
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Menú Principal"
         Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -127,4 +183,9 @@ Partial Class MenuPrincipal
     Friend WithEvents BTN_SALIR As Button
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents CMB_SUCURSAL As ComboBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TXT_VENTA As TextBox
+    Friend WithEvents TXT_COMPRA As TextBox
 End Class
